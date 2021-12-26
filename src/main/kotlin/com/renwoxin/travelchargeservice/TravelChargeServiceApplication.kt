50251a0f293e6dc58ffae08ae9db1e2fea51196f
jiangze.mei <jiangze.mei@thoughtworks.com>
2021-12-26 12:46:19 +0800

    infra [Jiangze] init project programing environment

---
+++ src/main/kotlin/com/renwoxin/travelchargeservice/TravelChargeServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.renwoxin.travelchargeservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TravelChargeServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<TravelChargeServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/renwoxin/travelchargeservice/TravelChargeServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.renwoxin.travelchargeservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TravelChargeServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
