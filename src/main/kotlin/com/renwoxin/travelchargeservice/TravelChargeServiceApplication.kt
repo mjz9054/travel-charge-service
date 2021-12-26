e6cfcc4117c723cc4a377c37aeef45d6c1259d79
jiangze.mei <jiangze.mei@thoughtworks.com>
2021-12-26 20:15:13 +0800

    feat [Jiangze] enable scheduling for schedule create invoice

--- src/main/kotlin/com/renwoxin/travelchargeservice/TravelChargeServiceApplication.kt
+++ src/main/kotlin/com/renwoxin/travelchargeservice/TravelChargeServiceApplication.kt
@@ -2,8 +2,10 @@ package com.renwoxin.travelchargeservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class TravelChargeServiceApplication
 
 fun main(args: Array<String>) {
---
+++ src/main/kotlin/com/renwoxin/travelchargeservice/schedule/InvoiceScheduler.kt
@@ -0,0 +1,18 @@
+package com.renwoxin.travelchargeservice.schedule
+
+import com.renwoxin.travelchargeservice.service.TravelInvoiceService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Service
+
+@Service
+class ScheduleService {
+
+    @Autowired
+    private lateinit var travelInvoiceService: TravelInvoiceService
+
+    @Scheduled(cron = "\${invoice.cron}")
+    fun scheduleCreateInvoice() {
+        travelInvoiceService.scheduleCreateInvoice()
+    }
+}
