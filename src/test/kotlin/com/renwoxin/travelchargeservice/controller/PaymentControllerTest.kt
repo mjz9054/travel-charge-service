9ef8dd0035c095a88669757cc6b7a798a495edc5
jiangze.mei <jiangze.mei@thoughtworks.com>
2021-12-26 18:36:26 +0800

    feat [Jiangze] test response when order has already been paid

--- src/test/kotlin/com/renwoxin/travelchargeservice/controller/PaymentControllerTest.kt
+++ src/test/kotlin/com/renwoxin/travelchargeservice/controller/PaymentControllerTest.kt
@@ -2,6 +2,8 @@ package com.renwoxin.travelchargeservice.controller
 
 import com.nhaarman.mockitokotlin2.whenever
 import com.renwoxin.travelchargeservice.dto.TravelChargePaymentDto
+import com.renwoxin.travelchargeservice.exception.BusinessException
+import com.renwoxin.travelchargeservice.exception.ErrorCode
 import com.renwoxin.travelchargeservice.service.TravelChargeService
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -45,6 +47,33 @@ class PaymentControllerTest {
         val actual = result.andReturn().response.contentAsString
         val expected = """
             {"data":{"status":"ACCEPT"},"success":true,"error":null}
+        """.trimIndent()
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `should return order has been paid when create payment given order already paid exception`() {
+        // given
+        val orderId = 93L
+        val createPaymentRequest = """
+            {
+                "orderId": 93
+            }
+        """.trimIndent()
+
+        whenever(travelChargeService.createPayment(orderId)).thenThrow(BusinessException(ErrorCode.TRAVEL_ORDER_ALREADY_PAID))
+
+        // when
+        val result = mvc.perform(
+            post("/travel-charge-orders/$orderId/payment")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(createPaymentRequest)
+
+        )
+        // then
+        val actual = result.andReturn().response.contentAsString
+        val expected = """
+            {"data":null,"success":false,"error":"Travel order has been paid already"}
         """.trimIndent()
         assertThat(actual).isEqualTo(expected)
     }
